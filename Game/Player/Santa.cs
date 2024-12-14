08c99d2b7db2f8babc3ee012d6a1a80f740a43bf
Piotr Zarzycki <pan.bobon@gmail.com>
2024-12-14 14:07:59 +0100

    Added snowball, and basio snowball movement

--- Game/Player/Santa.cs
+++ Game/Player/Santa.cs
@@ -5,36 +5,62 @@ public partial class Santa : CharacterBody2D
 {
     private Vector2 _velocity = Vector2.Zero;
     [Export] AnimatedSprite2D _animatedSprite;
+    
+    // Movement Variables
     [Export] public float Speed = 200f;
     [Export] public float Gravity = 500f;
     [Export] public float JumpForce = 100f;
     [Export] public float Friction = 800f;
+    
+    // Snowball Variables
+    [Export] public PackedScene SnowballScene;
+    bool _canThrow = true;
 
     public override void _PhysicsProcess(double delta)
     {
         _velocity = Velocity;
 
+        // Gravity
         if (!IsOnFloor())
         {
             ApplyGravity(delta);
         }
         
+        // Jump
         if (Input.IsActionPressed("jump") && IsOnFloor())
         {
             ApplyJump();
         }
         
+        
+        // Movement
         float inputDirection = Input.GetAxis("move_left", "move_right");
         ApplyMovement(inputDirection, delta);
         
+        
+        // Throw snowball
+        if (Input.IsActionJustPressed("throw") && _canThrow)
+        {
+            ThrowSnowball();
+        }
+        
+        // Update Animation
         UpdateAnimation();
+        
         
         Velocity = _velocity;
         MoveAndSlide();
         
         
     }
-    
+
+    private void ThrowSnowball()
+    {
+        // Create snowball
+        var snowball = (Snowball)GD.Load<PackedScene>("res://Scenes/Snowball.tscn").Instantiate();
+        GetTree().Root.AddChild(snowball);
+    }
+
 
     private void ApplyJump()
     {
