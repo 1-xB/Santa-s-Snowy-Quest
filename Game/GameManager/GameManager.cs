486d21d3a16246b39681f51fb45b18825ddc3f68
Piotr Zarzycki <pan.bobon@gmail.com>
2024-12-20 20:02:53 +0100

    Add pause menu functionality with resume, restart, and main menu options

--- Game/GameManager/GameManager.cs
+++ Game/GameManager/GameManager.cs
@@ -4,7 +4,17 @@ using System;
 public partial class GameManager : Node
 {
     [Export] private HBoxContainer _livesContainer;
+    [Export] CanvasLayer _pauseMenu;
 
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("escape"))
+        {
+            _pauseMenu.Visible = !_pauseMenu.Visible;
+            GetTree().Paused = _pauseMenu.Visible;
+        }
+    }
     public void UpdateHearts(int lives)
     {
         for (int i = 0; i < _livesContainer.GetChildren().Count; i++)
@@ -18,6 +28,26 @@ public partial class GameManager : Node
                 _livesContainer.GetChild<Control>(i).Visible = false;
             }
         }
+    }
+
+    public void RestartLevel()
+    {
+        _pauseMenu.Visible = false;
+        GetTree().Paused = false;
+        GetTree().ReloadCurrentScene();
+    }
+
+    public void GoToMainMenu()
+    {
+        _pauseMenu.Visible = false;
+        GetTree().Paused = false;
+        GetTree().ChangeSceneToFile("res://Scenes/StartMenu.tscn");
+    }
+
+    public void ResumeGame()
+    {
+        _pauseMenu.Visible = false;
+        GetTree().Paused = false;
     }
     
 }
