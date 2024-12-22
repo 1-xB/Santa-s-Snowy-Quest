2a0daeb3d9431145370ce26b9de323486559a2ab
Piotr Zarzycki <pan.bobon@gmail.com>
2024-12-22 11:58:42 +0100

    Add coin collection and saving functionality, including UI updates and victory condition

--- Game/Coin/Coin.cs
+++ Game/Coin/Coin.cs
@@ -3,11 +3,18 @@ using System;
 
 public partial class Coin : Node2D
 {
+
+    [Export] Label label;
+    private int _coins = 0;
     private void OnBodyEntered(Node body)
     {
+        
         // TODO : Logika dodania coina
         if (body is Santa)
         {
+            _coins = label.Text.ToInt();
+            _coins++;
+            label.Text = _coins.ToString();
             QueueFree();
         }
     }
