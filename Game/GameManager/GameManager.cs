78bd5f8bcc6adb6d84480b47754726c4f49ea544
Piotr Zarzycki <pan.bobon@gmail.com>
2024-12-22 11:58:42 +0100

    Add coin collection and saving functionality, including UI updates and victory condition

--- Game/GameManager/GameManager.cs
+++ Game/GameManager/GameManager.cs
@@ -7,7 +7,19 @@ public partial class GameManager : Node
     [Export] CanvasLayer _pauseMenu;
     
     [Export] AnimationPlayer _animationPlayer;
+    [Export] Label _coinLabel;
 
+    private int _coins = 0;
+    private string _saveSavePath = "user://data.cfg";
+    private ConfigFile _configFile = new ConfigFile();
+
+    public override void _Ready()
+    {
+        _pauseMenu.Visible = false;
+        GetTree().Paused = false;
+        
+        LoadCoins();
+    }
     public override void _Process(double delta)
     {
         if (Input.IsActionJustPressed("escape"))
@@ -55,5 +67,54 @@ public partial class GameManager : Node
     {
         _animationPlayer.Play("FadeOut");
     }
+
+    public void SaveCoins()
+    {
+        // https://www.youtube.com/watch?v=tfqJjDw0o7Y
+        _coins = _coinLabel.Text.ToInt();
+        _configFile.SetValue("player", "coins", _coins);
+        Error error = _configFile.Save(_saveSavePath);
+        if (error != Error.Ok)
+        {
+            GD.Print("Error saving file");
+        }
+        else 
+        {
+            GD.Print("File saved");
+        }
+    }
+
+    public void LoadCoins()
+    {
+        // https://www.youtube.com/watch?v=tfqJjDw0o7Y
+        Error error = _configFile.Load(_saveSavePath);
+        if (error == Error.Ok)
+        {
+            _coins = (int)_configFile.GetValue("player", "coins");
+            _coinLabel.Text = _coins.ToString();
+        }
+        else
+        {
+            _coins = 0;
+            _coinLabel.Text = _coins.ToString();
+            GD.Print("Error loading file");
+        }
+        
+    }
+
+    public void Victory()
+    {
+        SaveCoins();
+        _animationPlayer.Play("FadeOut");
+
+    }
+
+    private void OnWinAreaBodyEntered(Node body)
+    {
+        if (body is Santa)
+        {
+            Victory();
+        }
+    }   
     
 }
